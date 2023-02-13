2b4816ce1fefeaf9fe8fc39cb003c474514db399
rogerburks <17787203+rogerburks@users.noreply.github.com>
2023-02-12 19:46:26 -0500

    Added a sort function for query results. Made the enter key work for searches

--- src/components/BinomialSearch.vue
+++ src/components/BinomialSearch.vue
@@ -1,11 +1,12 @@
 <!-- in vue.js, ideally displayed information should be reactive and a single page should handle as much as possible-->
 <!-- This component handles searches and displays search results -->
+<!-- //To do: add checkbox switch for including or excluding OTUs from the nomenclature search -->
 <template>
     <div class="input-group mb-3" id="binomial-search-group">
         <span class="input-group-text" id="genus-input-label">genus</span>
-        <input type="text" class="form-control" aria-describedby="genus-input" v-model="genus">
+        <input type="text" class="form-control" aria-describedby="genus-input" v-model="genus" @keyup.enter="useInputTerms($event)">
         <span class="input-group-text" id="species-input-label">species</span>
-        <input type="text" class="form-control" aria-describedby="species-input" v-model="species">
+        <input type="text" class="form-control" aria-describedby="species-input" v-model="species" @keyup.enter="useInputTerms($event)">
         <button class="btn btn-outline-secondary" type="button" id="binomial-search-button" @click="useInputTerms($event)">search</button>
     </div>
     <br>
@@ -21,13 +22,13 @@
     </div>
     -->
     <div class="row align-items-start">
-      <div class="col p-2 bd-highlight" id="results-list-div" ref="resultsList">
+      <div class="col-xl-8 bd-highlight" id="results-list-div" ref="resultsList">
         <span id="results-list-span"><br>
         <li style="list-style-type:none" v-for="(item, index) in apiResults" :key="item.id"><a style="text-decoration:underline; color: var(--bs-link-color);" @click="displayTaxonPage(apiResults[index])"><i>{{ apiResults[index].cached }}</i> {{ apiResults[index].cached_author_year }}</a></li>
         </span>
       </div>
       
-      <div class="col p-2 bd-highlight" id="taxon-page-div" ref="taxonPage">
+      <div class="col-xl-8 bd-highlight" id="taxon-page-div" ref="taxonPage">
         <div>
           <span id="taxon-page-italicized-name" style="font-size:large; font-style: italic; font-weight: 600;" ref="taxonPageNameItalicized"><strong></strong> </span><span id="taxon-page-author-year" style="font-size:large; font-style: normal; font-weight: 600;" ref="taxonPageNameAuthorYear"></span>
         </div>
@@ -70,17 +71,18 @@ export default {
             apiResults: ref([{}]),
             listClickedText: ref(''),
             taxonClicked: ref([{}]),
-            taxonIDClicked: ref('')
+            taxonIDClicked: ref(''),
+            sortedResponse: ref([{}])
             }
     
   },
   
   methods: {
-    //this handles button presses
+    //this handles button presses for nomenclature searches
     useInputTerms(e) {
       //console.log(this.genus);
       //console.log(this.species);
-      
+      //If a genus and a species are entered, this functions as an exact search. If only a genus or a species is entered, it returns whatever matches
       if(this.genus && this.species){
         this.genus = this.genus.replace(/^./, this.genus[0].toUpperCase());
         this.species = this.species.replace(/./, this.species[0].toLowerCase());
@@ -92,20 +94,25 @@ export default {
       else if(this.genus){
         this.genus = this.genus.replace(/^./, this.genus[0].toUpperCase());
         this.apiResults = axios
-        .get('https://sfg.taxonworks.org/api/v1/taxon_names?name=' + this.genus + '&validity=true&exact=true&token=e1KivaZS6fvxFYVaqLXmCA&project_token=adhBi59dc13U7RxbgNE5HQ')
-        .then(response => {(this.apiResults = response.data)})
+        .get('https://sfg.taxonworks.org/api/v1/taxon_names?name=' + this.genus + '&validity=true&exact=false&token=e1KivaZS6fvxFYVaqLXmCA&project_token=adhBi59dc13U7RxbgNE5HQ')
+        .then(response => {
+          this.apiResults = response.data
+          this.sortResponse()
+          }
+        )
         .catch(error => console.log(error))
       }
       else if(this.species){
         this.species = this.species.replace(/^./, this.species[0].toLowerCase());
         this.apiResults = axios
       .get('https://sfg.taxonworks.org/api/v1/taxon_names?name=' + this.species + '&token=e1KivaZS6fvxFYVaqLXmCA&project_token=adhBi59dc13U7RxbgNE5HQ')
-      .then(response => {(this.apiResults = response.data)})
+      .then(response => {
+          this.apiResults = response.data
+          this.sortResponse()
+          }
+        )
       .catch(error => console.log(error))
       }
-      //if (this.apiResults){
-      //  this.genus = ""
-      //}
       this.$refs['taxonPageNameItalicized'].textContent = ''
       this.$refs['taxonPageNameAuthorYear'].textContent = ''
     },
@@ -118,6 +125,15 @@ export default {
       console.log(taxonClicked)
       this.$refs['taxonPageNameItalicized'].textContent = taxonClicked.cached + " "
       this.$refs['taxonPageNameAuthorYear'].textContent = taxonClicked.cached_author_year
+    },
+    
+    sortResponse() {
+      //this.apiResults = this.apiResults.sort(function(a, b){return a.cached - b.cached})
+      this.sortedResponse = this.apiResults.sort((a, b) =>{
+        if (a.cached < b.cached) return -1
+        if (a.cached > b.cached) return 1
+        return 0
+      })
     }
   }
 }
