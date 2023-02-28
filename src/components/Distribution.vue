0c67393d7e5e46d92bdb53429cc16f8b0c3f3340
rogerburks <17787203+rogerburks@users.noreply.github.com>
2023-02-28 17:39:55 -0500

    Added some basic components, adapted the request to start making chains of otu IDs

---
+++ src/components/Distribution.vue
@@ -0,0 +1,10 @@
+<template>
+    
+</template>
+
+<script>
+  import axios from "axios"
+  import { computed, ref, reactive } from '@vue/runtime-core'
+</script>
+
+https://sfg.taxonworks.org/asserted_distributions.json?otu_id=101968&embed[]=shape&extend[]=geographic_area
