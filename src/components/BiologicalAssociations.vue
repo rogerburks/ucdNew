937a17861ce96b90742b1d2f11c6d332a566e2a1
rogerburks <17787203+rogerburks@users.noreply.github.com>
2023-05-20 19:28:25 -0700

    made biological associations properly reactive

--- src/components/BiologicalAssociations.vue
+++ src/components/BiologicalAssociations.vue
@@ -36,50 +36,42 @@
     data(){
       return {
         showBiologicalAssociations: true,
+        biologicalAssociationsJson: []
       }
     },
     
     computed: {
-        biologicalAssociationsResultsExist() {
-          return this.$route.query.taxonID !== undefined && this.$route.query.taxonID !== null;
+      sortedBiologicalAssociations() {
+        return this.biologicalAssociationsJson
+        .filter(association => association.object_tag)
+        .map(association => association.object_tag.toString());
+      },
+      biologicalAssociationsResultsExist() {
+        return this.sortedBiologicalAssociations && this.sortedBiologicalAssociations.length > 0;
       }
     },
     
-    setup() {
-      const biologicalAssociationsJson = ref([{}]);
-      
-      //const finalBiologicalAssociationsArray = computed(() => {
-      //  return [...biologicalAssociationsJson.object_tag.value]
-      //});
-      
-      return {
-        biologicalAssociationsJson
-        //finalBiologicalAssociationsArray
-      };
-    },
-    
-    async mounted() {
-      //if(taxonID){
-      //  const response = await axios.get(`https://sfg.taxonworks.org/api/v1/taxon_names?taxon_name_id[]=${taxonID}&validity=true&per=250&exact=true&token=e1KivaZS6fvxFYVaqLXmCA&project_token=adhBi59dc13U7RxbgNE5HQ`);
-      //  https://sfg.taxonworks.org/tasks/biological_associations/filter?otu_id[]=118743&otu_id[]=129683&otu_id[]=129684&otu_id[]=129726&extend%5B%5D=object&extend%5B%5D=subject&extend%5B%5D=biological_relationship&extend%5B%5D=taxonomy&extend%5B%5D=biological_relationship_types&page=1
-      //  const relationshipsResponse = await axios.get(`https://sfg.taxonworks.org/api/v1/taxon_name_relationships?object_taxon_name_id=${taxonID}&token=e1KivaZS6fvxFYVaqLXmCA&project_token=adhBi59dc13U7RxbgNE5HQ`);
-        
-      //  this.taxonViewed = await response.data
-     // }
-      const promises = [];
+  async mounted() {
+    await this.fetchBiologicalAssociations();
+  },
+  
+  methods: {
+    async fetchBiologicalAssociations() {
       if (this.baProp){
-        console.log("this.baprop is: " + this.baProp)
         const baResponse = await axios.get(`https://sfg.taxonworks.org/api/v1/biological_associations?${this.baProp}&extend[]=object&extend[]=subject&extend[]=biological_relationship&extend[]=taxonomy&extend[]=biological_relationship_types&page=1&token=e1KivaZS6fvxFYVaqLXmCA&project_token=adhBi59dc13U7RxbgNE5HQ`);
-        this.biologicalAssociationsJson = await baResponse.data
-        console.log("biological associations are: " + this.biologicalAssociationsJson)
-        
-        this.sortedBiologicalAssociations = []
-        for (const association in this.biologicalAssociationsJson){
-          if(this.biologicalAssociationsJson[association].object_tag){
-            this.sortedBiologicalAssociations.push(this.biologicalAssociationsJson[association].object_tag.toString())
-          }
-        }
+        let newData = await baResponse.data;
+
+        // Make sure Vue's reactivity system acknowledges the change
+        this.biologicalAssociationsJson = [];
+        this.biologicalAssociationsJson = newData;
       }
+    },
+  },
+  
+  watch: {
+    async baProp() {
+      await this.fetchBiologicalAssociations();
     }
+  },
 }
 </script>
