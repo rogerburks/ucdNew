6c5dc804a3200bdfbe9e00ada286e2ea2325644e
rogerburks <17787203+rogerburks@users.noreply.github.com>
2023-07-09 17:58:39 -0700

    Added a TaxonWorks svg footer

--- src/App.vue
+++ src/App.vue
@@ -11,21 +11,45 @@
 </script>
 
 <template>
-  <div class="banner" src="./components/images/chalcidBanner.jpg" alt="Banner Image"></div>
-  <div id="app">
-    <LinkRow></LinkRow>
-    <router-view :key="$route.fullPath"></router-view>
+  <div class="wholeHeight">
+    <div class="mainContent">
+      <div class="banner" src="./components/images/chalcidBanner.jpg" alt="Banner Image"></div>
+      <div id="app">
+        <LinkRow></LinkRow>
+        <router-view :key="$route.fullPath"></router-view>
+      </div>
+    </div>
+    <div class="footer"><span>Powered by: <a href="http://taxonworks.org/"><img src="/taxonworksImage.svg" /></a></span></div>
   </div>
 </template>
 
 <style scoped>
+.wholeHeight {
+  display: flex;
+  flex-direction: column;
+  min-height: 95vh;
+}
+
+.mainContent {
+  flex: 1 0 auto;
+}
+
 header {
   line-height: 1.5;
 }
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
+.footer {
+  display: flex;
+  flex-shrink: 0;
+  justify-content: space-between;
+  align-items: center;
+  align-self: center;
+  width: 100%;
+  height: 50px;
+}
+
+.footer img {
+  height: 50px;
 }
 
 @media (min-width: 1024px) {
@@ -34,17 +58,7 @@ header {
     place-items: center;
     padding-right: calc(var(--section-gap) / 2);
   }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: nowrap;
-  }
-  
+    
   .banner {
     width: 100%;
     height: 200px;
@@ -53,11 +67,7 @@ header {
     background-repeat: repeat-x;
     background-position: left;
   }
-  
-  .nav-item {
-    display: inline-block;
-  }
-  
+    
   #app {
     padding: 0%
   }
